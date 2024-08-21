ede0eaff966f1cbe22259da2367c15252eaea5a5
HrishikeshPK <hrishilovjith@gmail.com>
2024-08-21 21:05:36 +0530

    V45

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,21 +1,11 @@
-import { useCallback, useEffect, useState } from "react"
-
+import {  useState } from "react"
+import { useFetch } from "../hooks/useFetch"
 
 export const ProductList = () => {
-    const [products, setProducts] = useState([])
+    
     const [url, setUrl] = useState("http://localhost:8000/products")
-
-    const fetchProducts = useCallback( async () => {
-        const response = await fetch(url)
-        const data = await response.json()
-        setProducts(data)
-    },[url])
-       
-
-    useEffect(() => {
-       fetchProducts()
-       console.log("-")
-    }, [fetchProducts])
+    const { data: products, loading, error } = useFetch(url, { content: "ABC"} )
+    
 
     return (
         <section>
@@ -23,7 +13,10 @@ export const ProductList = () => {
            <button onClick={() => setUrl("http://localhost:8000/products")}>All</button>
            <button onClick={() => setUrl("http://localhost:8000/products?in_stock=true")}>In Stock Only</button>
            </div>
-            {products.map((product) => (
+           {loading && <p>Loading products</p> }
+            {error && <p>{error}</p>}
+
+            { products && products.map((product) => (
                 <div className="card" key={product.id}>
                     <p className="id">{product.id}</p>
                     <p className="name">{product.name}</p>
